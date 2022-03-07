abf0da0e6778e436bed11d0fcc70243fdf90d89b
Laudemir <nlaudemir@gmail.com>
2022-03-07 20:50:38 -0300

    feat: finish home

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { Container } from "./styles";
 
-export default function Button() {
-  return <div>Button</div>;
+export default function Button({ children, ...rest }) {
+  return <Container {...rest}>{children}</Container>;
 }
