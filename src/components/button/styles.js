90d1369f9f2f620f58c29c054eb581401076a99c
Laudemir <nlaudemir@gmail.com>
2022-03-10 14:39:57 -0300

    fix: release main

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -6,6 +6,6 @@ export const Container = styled.button`
   font-size: 24px;
   color: var(--white);
   border-radius: 20px;
-  padding: 0 20px;
+  padding: 0 25px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); ;
 `;
